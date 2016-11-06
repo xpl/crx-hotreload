4dbb0e7e5dbb54df30e6ad5c45ae0f4ffbdc7ede
Vitaly Gordon <rocket.mind@gmail.com>
2016-11-06 22:51:56 +0300

    Create hot-reload.js

---
+++ hot-reload.js
@@ -0,0 +1,50 @@
+const watchChanges = (files, prevTexts) => {
+
+    Promise.all (files.map (url => fetch (new Request (chrome.extension.getURL (url))).then (r => r.text ())))
+           .then (sourceTexts => {
+
+                chrome.tabs.query ({ active: true, currentWindow: true }, tabs => {
+
+                    let changed = false
+
+                    if (prevTexts) {
+                        sourceTexts.forEach ((text, i) => {
+                            changed = changed || (text !== prevTexts[i])
+                        })
+                    }
+
+                    if (changed) {
+                    
+                            chrome.tabs.sendMessage (tabs[0].id, { action: 'runtime_reloaded' }, () => {})
+                            chrome.runtime.reload ()
+
+                    } else {
+
+                        setTimeout (() => watchChanges (files, sourceTexts), 1000)
+                    }
+                })
+                
+            })
+}
+
+chrome.runtime.getPackageDirectoryEntry (entry => {
+
+    entry.createReader ().readEntries (entries => {
+
+        const urls = []
+
+        entries.forEach (e => {
+
+            const ext = e.name.split ('.').pop ()
+
+            if (ext === 'js' ||
+                ext === 'json') {
+
+                urls.push ('/' + e.name)
+            }
+            
+        })
+
+        watchChanges (urls)
+    });
+})
