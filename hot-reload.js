1d8b176c0c399bb884377e7fa01345fd85718d52
drewio <drewio@users.noreply.github.com>
2017-10-10 14:55:29 +1100

    Fixed "Extension reloaded itself too frequently"
    See https://stackoverflow.com/questions/29086691/chrome-this-extension-loaded-itself-too-frequently,
    and https://chromium.googlesource.com/chromium/src/+/e9d7496ee1703754c5d56027aa2758a09c473c0a

--- hot-reload.js
+++ hot-reload.js
@@ -18,13 +18,7 @@ const timestampForFilesInDirectory = dir =>
             files.map (f => f.name + f.lastModifiedDate).join ())
 
 const reload = () => {
-
-    chrome.tabs.query ({ active: true, currentWindow: true }, tabs => {
-
-        if (tabs[0]) { chrome.tabs.reload (tabs[0].id) }
-
-        chrome.runtime.reload ()
-    })
+    chrome.developerPrivate.reload(chrome.runtime.id, {failQuietly: true});
 }
 
 const watchChanges = (dir, lastTimestamp) => {
