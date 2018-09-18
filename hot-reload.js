6025184f5d8154ad3315eb0a6a7aaf91c170b733
Vitaly Gordon <rocket.mind@gmail.com>
2018-09-18 20:06:44 +0300

    A comment referencing the issue #5

--- hot-reload.js
+++ hot-reload.js
@@ -19,7 +19,7 @@ const timestampForFilesInDirectory = dir =>
 
 const reload = () => {
 
-    chrome.tabs.query ({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.query ({ active: true, currentWindow: true }, tabs => { // NB: see https://github.com/xpl/crx-hotreload/issues/5
 
         if (tabs[0]) { chrome.tabs.reload (tabs[0].id) }
 
