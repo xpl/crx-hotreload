09c418a7aa740f726b98bd4f6d9992d63646eddf
Vitaly Gordon <rocket.mind@gmail.com>
2018-09-18 15:01:09 +0300

    currentWindow: true

--- hot-reload.js
+++ hot-reload.js
@@ -19,7 +19,7 @@ const timestampForFilesInDirectory = dir =>
 
 const reload = () => {
 
-    chrome.tabs.query ({ active: true, currentWindow: false }, tabs => {
+    chrome.tabs.query ({ active: true, currentWindow: true }, tabs => {
 
         if (tabs[0]) { chrome.tabs.reload (tabs[0].id) }
 
